f8faca352fbe6859cfdca1c5a8f5e1500a8921a8
Thiago Farina <tfarina@chromium.org>
2014-03-09 18:00:48 -0300

    Add the necessary boilerplate to connect to the FIX engine.

--- win/csharp/client/TradeClient/TradeClient/FIXApplicationImpl.cs
+++ win/csharp/client/TradeClient/TradeClient/FIXApplicationImpl.cs
@@ -5,6 +5,22 @@ namespace TradeClient
 {
     class FIXApplicationImpl : MessageCracker, Application
     {
+        SessionSettings session_settings_;
+        MessageStoreFactory message_store_factory_;
+        FileLogFactory file_log_factory_;
+        MessageFactory message_factory_;
+        ThreadedSocketInitiator socket_initiator_;
+
+        public void Init(string config_path)
+        {
+            session_settings_ = new SessionSettings(config_path);
+            message_store_factory_ = new FileStoreFactory(session_settings_);
+            file_log_factory_ = new FileLogFactory(session_settings_);
+            message_factory_ = new DefaultMessageFactory();
+            socket_initiator_ = new ThreadedSocketInitiator(this, message_store_factory_, session_settings_, file_log_factory_, message_factory_);
+            socket_initiator_.start();
+        }
+
         public void fromAdmin(Message msg, SessionID value)
         {
             throw new NotImplementedException();
