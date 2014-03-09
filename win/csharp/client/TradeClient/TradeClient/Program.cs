0bf86c41488ff858e6122babecf686468b5fbc2e
Thiago Farina <tfarina@chromium.org>
2014-03-09 18:06:40 -0300

    Check the number of arguments passed on command line.
    Exit the application if the user didn't pass the path to config file and the symbol.

--- win/csharp/client/TradeClient/TradeClient/Program.cs
+++ win/csharp/client/TradeClient/TradeClient/Program.cs
@@ -6,6 +6,11 @@ namespace TradeClient
     {
         static void Main(string[] args)
         {
+            if (args.Length != 3)
+            {
+                Console.WriteLine("usage: TradeClient.exe config-file SYMBOL");
+                Environment.Exit(-1);
+            }
             Console.WriteLine("Press [ENTER] to quit...");
             Console.ReadLine();
         }
