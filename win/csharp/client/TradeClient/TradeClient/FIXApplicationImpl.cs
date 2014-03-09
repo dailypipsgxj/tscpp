8b259495076f224704381e1bc55731ad8917e277
Thiago Farina <tfarina@chromium.org>
2014-03-09 17:42:28 -0300

    Initial Commit of CSharp port.
    This will be used to port it to Windows since the C++ part requires ATL which isn't available in the
    Express version of VS 2010.

---
+++ win/csharp/client/TradeClient/TradeClient/FIXApplicationImpl.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using QuickFix;
+
+namespace TradeClient
+{
+    class FIXApplicationImpl : MessageCracker, Application
+    {
+        public void fromAdmin(Message msg, SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void fromApp(Message msg, SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void onCreate(SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void onLogon(SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void onLogout(SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void toAdmin(Message msg, SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void toApp(Message msg, SessionID value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
