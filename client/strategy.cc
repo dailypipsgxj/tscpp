29920a8bde459958612c659a9e69730592bdb58a
Thiago Farina <tfarina@chromium.org>
2014-03-08 23:40:47 -0300

    Calculate the Vwap and use it to decide whether to buy or sell.

--- client/strategy.cc
+++ client/strategy.cc
@@ -7,5 +7,15 @@ Strategy::Strategy(const std::string& symbol) : symbol_(symbol), vwap_(3) {}
 void Strategy::DumpTrade(double price, double quantity) {
   vwap_.AddTrade(price, quantity);
 
+  const double current_vwap = vwap_.Calculate();
+
+  printf("VWAP %lu = %.2f\n", vwap_.num_trades(), current_vwap);
+
   printf("%.2f %.0f\n", price, quantity);
+
+  if (price < (current_vwap - 1)) {
+    printf("SELL!!!\n");
+  } else if (price > (current_vwap + 1)) {
+    printf("BUY!!!\n");
+  }
 }
